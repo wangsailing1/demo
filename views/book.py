bdf84b912d561d2945bb3515bb32a07862374c4a
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-09-12 11:25:50 +0800

    change

--- views/book.py
+++ views/book.py
@@ -2,35 +2,45 @@
 from tools.gift import add_mult_gift
 from gconfig import game_config
 
+
 def card_book_index(hm):
     mm = hm.mm
-    return 0, {'data':mm.card_book.book}
+    data = {}
+    data['cards'] = mm.card_book.cards
+    data['data'] = {i: {'flag': j['flag']} for i, j in mm.card_book.book.iteritems()}
+    return 0, data
+
 
 def get_card_reward(hm):
     mm = hm.mm
-    group_id = int(hm.get_argument('group_id',0))
+    group_id = int(hm.get_argument('group_id', 0))
     if group_id not in mm.card_book.book:
-        return 1, {}  #组合未完成
+        return 1, {}  # 组合未完成
     if mm.card_book.book[group_id]['flag'] == -1:
-        return 2, {}  #组合未完成
+        return 2, {}  # 组合未完成
     if mm.card_book.book[group_id]['flag'] == 1:
-        return 3, {}  #奖励已领
+        return 3, {}  # 奖励已领
     config = game_config.card_book
     if group_id not in config:
-        return 4, {}  #没有相应组合
+        return 4, {}  # 没有相应组合
     gift = config[group_id]['award']
     mm.card_book.book[group_id]['flag'] = 1
-    reward = add_mult_gift(mm,gift)
+    reward = add_mult_gift(mm, gift)
     if not reward:
         return 5, {}
     mm.card_book.save()
 
-    return {'reward':reward,
-            'data':mm.card_book.book}
+    return {'reward': reward,
+            'data': mm.card_book.book}
+
 
 def script_book_index(hm):
     mm = hm.mm
-    return 0, mm.script_book.book
+    data = {}
+    data['scripts'] = mm.script_book.scripts
+    data['data'] = {i: {'flag': j['flag']} for i, j in mm.script_book.book.iteritems()}
+    data['group'] = mm.script_book.group
+    return 0, data
 
 
 def get_script_reward(hm):
@@ -53,12 +63,7 @@ def get_script_reward(hm):
     mm.script_book.save()
 
     return 0, {'reward': reward,
-            'data': mm.script_book.book}
-
-
-def script_group_index(hm):
-    mm = hm.mm
-    return 0, mm.script_group.group
+               'data': mm.script_book.book}
 
 
 def get_group_reward(hm):
@@ -81,5 +86,4 @@ def get_group_reward(hm):
     mm.script_group.save()
 
     return 0, {'reward': reward,
-            'data': mm.script_group.group}
-
+               'data': mm.script_group.group}
