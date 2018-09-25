e38f647c11e936fa67f1b98f658f9e8c6369993c
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-09-25 15:44:01 +0800

    change

--- views/ranking_list.py
+++ views/ranking_list.py
@@ -3,83 +3,91 @@ from lib.core.environ import ModelManager
 from gconfig import game_config
 
 
+rank_mapping = {1:'appeal_rank',2:'output_rank',3:'alloutput_rank'}
+
 def rank_index(hm):
     mm = hm.mm
+    rank_id = int(hm.get_argument('rank_id',1))
+    ar = mm.get_obj_tools(rank_mapping[rank_id])
 
-    ar = mm.get_obj_tools('appeal_rank')
-    otr = mm.get_obj_tools('output_rank')
-    aotr = mm.get_obj_tools('alloutput_rank')
-
-    appeal_rank = ar.get_all_user(withscores=True)
-    output_rank = otr.get_all_user(withscores=True)
-    alloutput_rank = aotr.get_all_user(withscores=True)
+    rank_list = ar.get_all_user(withscores=True)
 
     appeal_rank_list = []
     output_rank_list = []
     alloutput_rank_list = []
 
-    for uid_group_id, score in appeal_rank:
-        uid, group_id = uid_group_id.split('|')
-        umm = ModelManager(uid)
-        print uid, group_id
-        group_id = int(group_id)
-        card_id = umm.card.group_ids[group_id]
-        cid = int(card_id.split('-')[0])
-        card_name = game_config.card_basis[cid]['name']
-        name = umm.user.name
-        appeal_rank_list.append({'uid': uid,
-                                 'name': name,
-                                 'group_id': group_id,
-                                 'score': score,
-                                 'card_name': card_name})
+    if rank_id == 1:
+        for uid_group_id, score in rank_list:
+            uid, group_id = uid_group_id.split('|')
+            umm = ModelManager(uid)
+            print uid, group_id
+            group_id = int(group_id)
+            card_id = umm.card.group_ids[group_id]
+            cid = int(card_id.split('-')[0])
+            card_name = game_config.card_basis[cid]['name']
+            name = umm.user.name
+            appeal_rank_list.append({'uid': uid,
+                                     'name': name,
+                                     'group_id': group_id,
+                                     'score': score,
+                                     'card_name': card_name})
+        return 0, {
+            'rank_list': appeal_rank_list,
+            'rank_own':[]
+        }
 
-    output_rank_own_list = []
-    for uid_script_id, score in output_rank:
-        uid, script_id = uid_script_id.split('|')
-        umm = ModelManager(uid)
-        name = umm.user.name
-        script_id = int(script_id)
-        if mm.uid in uid_script_id:
-            output_rank_own_list.append({'uid': uid,
-                                         'name': name,
-                                         'script_id': script_id,
-                                         'score': score,
-                                         'uid_script_id': uid_script_id})
-        output_rank_list.append({'uid': uid,
-                                 'name': name,
-                                 'script_id': script_id,
-                                 'score': score, })
+    elif rank_id == 2:
+        output_rank_own_list = []
+        for uid_script_id, score in rank_list:
+            uid, script_id = uid_script_id.split('|')
+            umm = ModelManager(uid)
+            name = umm.user.name
+            script_id = int(script_id)
+            if mm.uid in uid_script_id:
+                output_rank_own_list.append({'uid': uid,
+                                             'name': name,
+                                             'script_id': script_id,
+                                             'score': score,
+                                             'uid_script_id': uid_script_id})
+            output_rank_list.append({'uid': uid,
+                                     'name': name,
+                                     'script_id': script_id,
+                                     'score': score, })
+        for uid_dict in output_rank_own_list:
+            uid = uid_dict['uid_script_id']
+            uid_dict['rank_own'] = ar.get_rank(uid)
 
-    alloutput_rank_own_list = []
-    for uid_group_id, score in alloutput_rank:
-        uid, group_id = uid_group_id.split('|')
-        umm = ModelManager(uid)
-        name = umm.user.name
-        group_id = int(group_id)
-        if mm.uid in uid_group_id:
-            alloutput_rank_own_list.append({'uid': uid,
-                                            'name': name,
-                                            'group_id': group_id,
-                                            'score': score,
-                                            'uid_group_id': uid_group_id})
-        alloutput_rank_list.append({'uid': uid,
-                                    'name': name,
-                                    'group_id': group_id,
-                                    'score': score, })
+        return 0, {
+            'rank_list': output_rank_list,
+            'rank_own': output_rank_own_list,
+        }
 
-    for uid_dict in output_rank_own_list:
-        uid = uid_dict['uid_script_id']
-        uid_dict['rank_own'] = otr.get_rank(uid)
-    for uid_dict in alloutput_rank_own_list:
-        uid = uid_dict['uid_group_id']
-        uid_dict['rank_own'] = aotr.get_rank(uid)
-    return 0, {
-        'appeal_rank': appeal_rank_list,
-        'output_rank_list': output_rank_list,
-        'output_rank_own': output_rank_own_list,
-        'alloutput_rank_list': alloutput_rank_list,
-        'alloutput_rank_own': alloutput_rank_own_list
-    }
+    else:
+        alloutput_rank_own_list = []
+        for uid_group_id, score in rank_list:
+            uid, group_id = uid_group_id.split('|')
+            umm = ModelManager(uid)
+            name = umm.user.name
+            group_id = int(group_id)
+            if mm.uid in uid_group_id:
+                alloutput_rank_own_list.append({'uid': uid,
+                                                'name': name,
+                                                'group_id': group_id,
+                                                'score': score,
+                                                'uid_group_id': uid_group_id})
+            alloutput_rank_list.append({'uid': uid,
+                                        'name': name,
+                                        'group_id': group_id,
+                                        'score': score, })
+
+
+        for uid_dict in alloutput_rank_own_list:
+            uid = uid_dict['uid_group_id']
+            uid_dict['rank_own'] = ar.get_rank(uid)
+        return 0, {
+            'rank_list': alloutput_rank_list,
+            'rank_own': alloutput_rank_own_list
+        }
 
 
 def get_script_info(hm):
