f51b80ab10be1a2582706ff10e4dc56aeaecd435
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-09-29 18:30:14 +0800

    change

--- models/book.py
+++ models/book.py
@@ -90,8 +90,8 @@ class ScriptBook(ModelBase):
                 'flag': -1  # 目标完成标识 -1 未完成 0 完成未领奖 1已领奖
             }
         self.group[group_id]['shoot_num'] += 1
-        if script_id > self.group[group_id]['max_script']:
-            self.group[group_id]['max_script'] = script_id
+        if script_config['sequel_count'] > self.group[group_id]['max_script']:
+            self.group[group_id]['max_script'] = script_config['sequel_count']
         if big_sale:
             self.group[group_id]['big_sale'] += 1
         self.group[group_id]['output'] += output
