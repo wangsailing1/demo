bc7e52ea2c22ee39e6f1dcccf35d603bf17cdb77
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-10-11 21:38:22 +0800

    change

--- models/block.py
+++ models/block.py
@@ -29,17 +29,14 @@ class Block(ModelBase):
 
         self.save()
 
-    #
-    def get_key(self):
-        return self.make_key(uid=self.block_num)
 
-    #
+    #获取玩家存储key
     def get_block_key(self):
         key = self.make_key(uid=self.block_num)
         key_date = key + '|' + self.get_date()
         return key_date
 
-    #获取日期
+    # 获取日期
     def get_date(self):
         now = time.strftime('%F')
         now_time = time.strftime('%T')
@@ -47,44 +44,49 @@ class Block(ModelBase):
             now = time.strftime('%F', time.localtime(time.time() + 3600 * 24))
         return now
 
-    #把玩家添加到所属街区
+    # 把玩家添加到所属街区
     def add_user_by_block(self, uid=None, score=0):
         if not uid:
             uid = self.uid
         key_date = self.get_block_key()
-        self.fredis.zadd(self._key_date, uid, score)
-        self.fredis.expire(self._key_date, 7 * 24 * 3600)
+        self.fredis.zadd(key_date, uid, score)
+        self.fredis.expire(key_date, 7 * 24 * 3600)
 
-    #从街区删除玩家（玩家升级街区后操作）
-    def delete_user_by_block(self, uid=None):
+    # 从街区删除玩家（玩家升级街区后操作）
+    def delete_user_by_block(self, uid=None,date = None,num = None):
+        if not num:
+            num = self.block_num - 1
+        if not date:
+            date = self.get_date()
         if not uid:
-            uid = self.block_num - 1
-        self._key = self.make_key(uid=uid)
-        self.fredis.zrem(self._key, self.uid)
+            uid = self.uid
+        key = self.make_key(uid=num)
+        key_date = key + '|' + date
+        self.fredis.zrem(key_date, uid)
 
-    #检查玩家是否在所属街区
+    # 检查玩家是否在所属街区
     def check_user_exist_by_block(self, uid=None):
         if not uid:
             uid = self.uid
-        return self.fredis.zscore(self._key_date,uid)
+        key_date = self.get_block_key()
+        return self.fredis.zscore(key_date, uid)
 
-    #获取编号
+    # 获取编号
     def get_num(self):
-        self._key_date = '%s|%s'%(self._key_date,self.NUM)
-        return self.fredis.incr(self._key_date)
+        key_date = self.get_block_key()
+        key_date = '%s|%s' % (key_date, self.NUM)
+        return self.fredis.incr(key_date)
 
-
-    #计算玩家所属组
-    def get_group(self,rank=None):
-        # if not uid:
-        #     uid = self.uid
-        #rank = self.fredis.zrank(self._key_date, uid)
+    # 计算玩家所属组
+    def get_group(self, uid=None):
+        if not uid:
+            uid = self.uid
+        rank = self.fredis.zrank(self._key_date, uid)
         if rank == 0:
             return 1
         if rank % 100 or not rank % 100 and rank / 100:
             return rank / 100 + 1
         return rank / 100
-
 
 
 ModelManager.register_model('block', Block)
