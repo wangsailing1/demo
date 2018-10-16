3c00e00ff895c5733b0b6afd11aa052889d657d0
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-10-16 10:24:09 +0800

    change

--- logics/block.py
+++ logics/block.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 –*-
 
-import time
 import random
 import copy
 
@@ -9,21 +8,20 @@ from lib.core.environ import ModelManager
 from models.block import get_date_before
 from gconfig import game_config, get_str_words
 
-rank_list = [1, 2, 3, 'nv', 'nan', 'medium', 'audience']
-
 
 class Block(object):
     def __init__(self, mm):
         self.mm = mm
+        self.block = self.mm.block
 
     def join_award_ceremony(self):
         data = {}
-        for tp in rank_list:
-            rank_uid = self.mm.block.get_key_profix(self.mm.block.block_num, self.mm.block.block_group,
+        for tp in self.block.rank_list:
+            rank_uid = self.block.get_key_profix(self.block.block_num, self.block.block_group,
                                                     tp)
             data.setdefault(tp, {'win': {}, 'nomination': {}})
-            date = time.strftime('%F')
-            br = BlockRank(rank_uid, self.mm.block._server_name, date)
+            date = get_date_before()
+            br = BlockRank(rank_uid, self.block._server_name, date)
             nomination = br.get_all_user(0, 4, withscores=True)
             if tp in ['nv', 'nan']:
                 id = 1
@@ -71,15 +69,15 @@ class Block(object):
                     id += 1
         big_sale_info = self.get_big_sale_info()
         data['big_sale_info'] = big_sale_info
-        self.mm.block.award_ceremony = 1
-        self.mm.block.save()
+        self.block.award_ceremony = 1
+        self.block.save()
         return data
 
     def get_big_sale_info(self):
         data = []
         check_data = {}
         date = get_date_before()
-        info = copy.deepcopy(self.mm.block.top_script.get(date, {}))
+        info = copy.deepcopy(self.block.top_script.get(date, {}))
         while len(data) < 10:
             for script_id, value in info.iteritems():
                 if value['big_sale_num'] >= 1:
@@ -102,3 +100,70 @@ class Block(object):
                 script_name = get_str_words(self.mm.user.language_sort, script_name)
                 robot_data.append([name, script_name])
         return {'own': data, 'robot': robot_data}
+
+
+    def get_reward(self):
+        self.count_cup()
+        data = {}
+        info = self.mm.block.reward_data
+        cup = 0
+        for k,v in info.iteritems():
+            if k == 'big_sale_cup':
+                cup += v
+            else:
+                cup += 1  #类型奖杯奖励读配置
+        data['old_block_num'] = self.block.block_num
+        data['old_block_group'] = self.block.block_group
+        self.block.get_award_ceremony = 1
+        self.block.up_block(cup)
+        key_uid = self.block.get_key_profix(self.block.block_num)
+        b = BlockRank(key_uid, self.block._server_name)
+        if not b.check_user_exist_by_block(self.mm.uid):
+            num = b.get_num()
+            b.add_user_by_block(self.mm.uid, num)
+            group = b.get_group(self.mm.uid)
+            self.block.block_group = group
+        data['new_block_num'] = self.block.block_num
+        data['new_block_group'] = self.block.block_group
+        data['cup'] = cup
+        self.block.save()
+
+        return data
+
+    def count_cup(self):
+        data = {}
+        for tp in self.block.rank_list:
+            rank_uid = self.block.get_key_profix(self.block.block_num, self.block.block_group,
+                                                    tp)
+            data.setdefault(tp, {})
+            date = get_date_before()
+            br = BlockRank(rank_uid, self.block._server_name, date)
+            nomination = br.get_all_user(0, 0, withscores=True)
+            if tp in ['nv', 'nan']:
+                for uid_card_id, score in nomination:
+                    uid, card_id = uid_card_id.split('_')
+                    if self.mm.uid != uid:
+                        continue
+                    card_cid = self.card.cards[card_id]['id']
+                    card_name = self.card.cards[card_id]['name']
+                    data[tp] = {
+                        'name': self.mm.user.name,
+                        'card_cid': card_cid,
+                        'card_name': card_name,
+                        'score': score
+                    }
+            else:
+                for uid_script_id, score in nomination:
+                    uid, script_id = uid_script_id.split('_')
+                    if self.mm.uid != uid:
+                        continue
+                    script_id = int(script_id)
+                    script_name = self.block.top_script.get(date, {}).get(script_id, {}).get('name', '')
+                    data[tp] = {
+                        'name': self.mm.user.name,
+                        'script_id': script_id,
+                        'script_name': script_name,
+                        'score': score
+                    }
+            data['big_sale_cup'] = self.block.big_sale
+        self.block.reward_data = data
