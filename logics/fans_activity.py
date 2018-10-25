8fdff65c291ba43494442dda5e69f05fa7b92b0c
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-10-25 10:11:49 +0800

    change

--- logics/fans_activity.py
+++ logics/fans_activity.py
@@ -1,6 +1,7 @@
 #! --*-- coding: utf-8 --*--
 
 from gconfig import game_config
+from tools.gift import add_mult_gift
 import time
 
 
@@ -21,8 +22,43 @@ class FansActivity(object):
         value = self.mm.fans_activity.activity_log.get(activity_id, {})
         all_time = config['time'] * 60
         remian_time = max(all_time + value.get('start_time', 0) - int(time.time()), 0)
-        if remian_time > 0:
-            return 18, {}  # 活动未结束
+        if remian_time > 0:  # 替换人物
+            for k, card_id in enumerate(cards):
+                if card_id in ['0']:
+                    continue
+                if card_id not in self.mm.card.cards:
+                    return 11, {}  # 卡牌错误
+                card_info = self.mm.card.get_card(card_id)
+                need = config['card_need']
+                for tp, need_num in need[k]:
+                    if tp <= 6:
+                        if card_info['all_char_pro'][tp - 1] < need_num:
+                            return 13, {}  # 有卡牌属性值不够
+                    elif tp == 7:
+                        if need_num != card_config[card_info['id']]['sex_type']:
+                            return 14, {}  # 有卡牌性别不符
+                    elif tp in [8, 9]:
+                        tp_key = self.mm.fans_activity.NEED_MAPPING[tp - 1]
+                        if need_num != card_config[card_info['id']][tp_key]:
+                            return 15, {}  # 有卡牌类型不符
+                    else:
+                        if self.mm.card.cards[card_id]['popularity'] < need_num:
+                            return 16, {}  # 有卡牌人气不足
+            gift = self.mm.fans_activity.count_produce(get_reward=True, activity_id=activity_id, is_save=False)
+            reward = add_mult_gift(self.mm, gift)
+            effect_id = self.mm.fans_activity.get_card_effect(cards)
+            self.mm.fans_activity.activity_log[activity_id]['cards'] = cards
+            self.mm.fans_activity.activity_log[activity_id]['effect_id'] = effect_id
+            self.mm.fans_activity.add_card_mapping()
+            self.mm.fans_activity.save()
+            _, data = self.fans_index()
+            data['reward'] = reward
+            return 0, data
+
+        # 活动已结束换人
+        if remian_time <= 0 and value.get('start_time', 0):
+            pass
+
         for k, card_id in enumerate(cards):
             if card_id in ['0']:
                 continue
@@ -51,7 +87,8 @@ class FansActivity(object):
             'gold_produce': {'last_time': now},
             'attention_produce': {'last_time': now},
             'cards': cards,
-            'effect_id':effect_id,
+            'effect_id': effect_id,
+            'reward': {}
         }
         self.mm.user.dollar -= cost
         self.mm.fans_activity.save()
@@ -61,7 +98,7 @@ class FansActivity(object):
 
     def fans_index(self, activity_id=0):
         config = game_config.fans_activity
-        data = {'activity_log':{}}
+        data = {'activity_log': {}}
         if activity_id:
             if activity_id not in self.mm.fans_activity.activity_log:
                 return 11, {}  # 未举办该活动
@@ -75,7 +112,7 @@ class FansActivity(object):
                 'items': items,
                 'remian_time': remian_time,
                 'cards': value['cards'],
-                'effect_id': value.get('effect_id',0),
+                'effect_id': value.get('effect_id', 0),
             }
             data['activity'] = self.mm.fans_activity.activity
             data['unlocked_activity'] = self.mm.fans_activity.unlocked_activity
