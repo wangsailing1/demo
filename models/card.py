00d3862f8b7fa09cf43ee9c4d9a8f4d25b716573
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-11-01 15:57:45 +0800

    change

--- models/card.py
+++ models/card.py
@@ -320,6 +320,9 @@ class Card(ModelBase):
         cur_lv = card_info['lv']
 
         card_config = game_config.card_basis[card_info['id']]
+        #count_lv 用于计算格调成长等级
+        count_lv = cur_lv - card_config['last_lv']
+
         base_char_pro = card_config['char_pro']
         grow_id = card_config['lv_growid']
         grow_config = game_config.card_level_grow[grow_id]
@@ -331,7 +334,9 @@ class Card(ModelBase):
             if base_pro > 0:
                 lv_grow_add = 0
                 # 公式确定后可以优化，只是奇偶数取不同列的话 复杂度可以做成常量的
-                for lv in xrange(2, cur_lv + 1):
+                for lv in xrange(1, count_lv + 1):
+                    if cur_lv == 1:
+                        continue
                     if lv % 2:
                         pro_grow_add = grow_config['pro_grow_odd'][idx]
                     else:
