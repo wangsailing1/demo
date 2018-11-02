e336bbcd2a61a92d8e62d25f0d80893e2c0c9317
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-11-02 20:12:34 +0800

    change

--- views/ranking_list.py
+++ views/ranking_list.py
@@ -58,7 +58,7 @@ def rank_index(hm):
                                   'card_name': card_name})
         return 0, {
             'rank_list': appeal_rank_list,
-            'rank_own': sorted(rank_own_list,key=lambda x:x['rank_own'],reverse=True)
+            'rank_own': sorted(rank_own_list,key=lambda x:x['rank_own'])
         }
 
     elif rank_id == 2:
@@ -246,7 +246,7 @@ def block_index(hm):
                                   'script_name': script_name})
         return 0, {
             'rank_list': script_list,
-            'rank_own': sorted(rank_own_list,key=lambda x:x['rank_own'],reverse=True),
+            'rank_own': sorted(rank_own_list,key=lambda x:x['rank_own']),
             'own_info': own_info
         }
     elif rank_id == 2:
