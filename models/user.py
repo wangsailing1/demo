9c2d8bab31697cf5986626a0d5fa21f9549b0b93
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-11-08 20:33:11 +0800

    change

--- models/user.py
+++ models/user.py
@@ -410,6 +410,7 @@ class User(ModelBase):
             div, mod = divmod(now - self.license_update_time, recover_need_time)
             while div and self.can_recover_license_times():
                 if self.script_license >= game_config.common[20]:
+                    self.license_update_time = int(time.time())
                     break
                 self.script_license += 1
                 self.license_recover_times += 1
@@ -422,8 +423,7 @@ class User(ModelBase):
                 if not recover_need_time:
                     break
                 div, mod = divmod(now - self.license_update_time, recover_need_time)
-                if self.script_license >= game_config.common[20]:
-                    self.license_update_time = int(time.time())
+
 
 
             if not self.can_recover_license_times():
