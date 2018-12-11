39b42af385717f60a333507c134dc8be35562528
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-12-11 18:48:55 +0800

    change

--- models/toy.py
+++ models/toy.py
@@ -5,6 +5,8 @@ from lib.db import ModelBase
 from lib.core.environ import ModelManager
 from gconfig import game_config
 from lib.utils import weight_choice
+from lib.utils import generate_rank_score, round_float_or_str
+import settings
 
 
 
@@ -16,12 +18,16 @@ class Toy(ModelBase):
             'toy_list': {},  # 奖池
             'version': 0,  # 版本号
             'toy_num': 0,  # 本次抓取次数
-            'all_toy_num': 0 # 总抓取次数
+            'all_toy_num': 0, # 总抓取次数
         }
+        server = self.get_server_name(self.uid)
+        father_server = settings.get_father_server(server)
+        self._key = self.make_key(self.__class__.__name__, server_name=father_server)
+        self.fredis = self.get_father_redis(father_server)
         super(Toy, self).__init__(self.uid)
 
     def pre_use(self):
-        version = 1
+        version = self.get_version()
         save = False
         if version != self.version:
             self.version = version
@@ -32,10 +38,17 @@ class Toy(ModelBase):
         if save:
             self.save()
 
+    def get_version(self):
+        return 1
+
     def init_reward(self, save=False):
-        config = game_config.rmb_gacha_control[1]
+        """
+        :param save: 
+        :return: 
+        """
+        config = game_config.rmb_gacha_control[self.version]
+        toy_reward_weight = game_config.toy_rmb_reward_weight_mapping()
         num = 1
-        toy_reward_weight = game_config.toy_reward_weight_mapping()
         for group_id, group_num in config['group_num']:
             for _ in range(group_num):
                 weight_config = toy_reward_weight[group_id]
@@ -45,13 +58,59 @@ class Toy(ModelBase):
         if save:
             self.save()
 
-    def refresh_reward(self,save=False):
+    def refresh_reward(self, save=False):
         self.init_reward()
         self.toy_num = 0
         if save:
             self.save()
 
+    def add_rank(self,uid,score):
+        self.fredis.zadd(self._key, uid, generate_rank_score(score))
+
+    def get_all_user(self, start=0, end=-1, withscores=False, score_cast_func=round_float_or_str):
+        return self.fredis.zrevrange(self._key, start, end, withscores=withscores, score_cast_func=score_cast_func)
+
+    def get_score(self, uid, score_cast_func=round_float_or_str):
+        score = self.fredis.zscore(self._key, uid) or 0
+        score = score_cast_func(score)
+
+        return score
+
+    def get_rank(self, uid):
+        rank = self.fredis.zrevrank(self._key, uid)
+        if rank is None:
+            rank = -1
+        return rank + 1
+
+class FreeToy(Toy):
+
+    def __int__(self,uid):
+        super(Toy, self).__init__(self.uid)
+
+    def get_version(self):
+        return 2
+
+    def init_reward(self, save=False):
+        """
+        :param save: 
+        :return: 
+        """
+        config = game_config.rmb_gacha_control[self.version]
+        toy_reward_weight = game_config.toy_rmb_reward_weight_mapping()
+        num = 1
+        for group_id, group_num in config['group_num']:
+            for _ in range(group_num):
+                weight_config = toy_reward_weight[group_id]
+                reward_id = weight_choice(weight_config)[0]
+                self.toy_list[num] = {'reward_id': reward_id, 'num': 0}
+                num += 1
+        if save:
+            self.save()
+
+
+
 ModelManager.register_model('toy', Toy)
+ModelManager.register_model('freetoy', FreeToy)
 
 
 
--- views/toy.py
+++ views/toy.py
@@ -3,9 +3,15 @@
 
 def index(hm):
     mm = hm.mm
+    sort = hm.get_argument('sort',is_int=True)
     return 0, {}
 
 def get_toy(hm):
     mm = hm.mm
     catch = hm.get_argument('catch',is_int=True)
+    sort = hm.get_argument('sort', is_int=True)
     reward_id = hm.get_argument('reward_id',is_int=True)
+
+def get_rank_reward(hm):
+    mm = hm.mm
+    sort = hm.get_argument('sort', is_int=True)
