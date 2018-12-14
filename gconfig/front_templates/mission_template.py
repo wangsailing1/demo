aff59c90a55a0b48739da2076f0df42bea51f8ef
jianmin.liu <jianmin.liu@kaiqigu.cn>
2018-12-14 15:29:31 +0800

    任务字段类型修改

--- gconfig/front_templates/mission_template.py
+++ gconfig/front_templates/mission_template.py
@@ -14,7 +14,7 @@ liveness = {
     'describe': ('describe', 'int'),  # 任务描述
     'liveness': ('liveness', 'int'),  # 活跃度
     'sort': ('sort', 'int'),  # 类型
-    'target': (('target1', 'target2', 'target3', 'target4', 'target5'), ('int', 'mult_force_num_list')),  # 目标
+    'target': (('target1', 'target2', 'target3', 'target4', 'target5'), ('int_list_or_int2', 'mult_force_num_list')),  # 目标
     'reward': ('reward', 'int_list'),  # 奖励
     'jump': ('jump', 'int_list'),  # 跳转
     'unlock_level': ('unlock_level', 'int_list'),  # 解锁等级
@@ -46,7 +46,7 @@ random_mission = {
     'uk': ('id', 'int'),
     'describe': ('describe', 'int'),  # 任务描述
     'sort': ('sort', 'int'),  # 类型
-    'target': (('target1', 'target2', 'target3', 'target4', 'target5'), ('int', 'mult_force_num_list')),  # 目标
+    'target': (('target1', 'target2', 'target3', 'target4', 'target5'), ('int_list_or_int2', 'mult_force_num_list')),  # 目标
     'reward': ('reward', 'int_list'),  # 奖励
     'jump': ('jump', 'int_list'),  # 跳转
     'weight': ('weight', 'int'),  # 权重
