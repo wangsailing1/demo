75d5bf322a628337f4da2fc97663db3f3369ccc0
jianmin.liu <jianmin.liu@kaiqigu.cn>
2019-01-04 11:19:03 +0800

    修改配置

--- gconfig/front_templates/build_template.py
+++ gconfig/front_templates/build_template.py
@@ -37,6 +37,6 @@ field = {
     'sort': ('sort', 'int'),    #
     'scaling': ('scaling', 'float'),    #
     'pose': ('pose', 'int_list'),
-    'build_scale': ('build_scale', 'int'),
+    'build_scale': ('build_scale', 'float'),
     'build_pos': ('build_pos', 'int_list'),
 }
