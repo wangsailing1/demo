57aac9d755ee22b08f4a0cd88a02c349b626c4fa
jianmin.liu <jianmin.liu@kaiqigu.cn>
2019-02-15 14:12:53 +0800

    修改导演

--- views/user.py
+++ views/user.py
@@ -148,6 +148,7 @@ def game_info(hm):
     info.update(dict(cards=mm.card.cards, pieces=mm.card.pieces))
     info.update(dict(equips=mm.equip.equips, equip_pieces=mm.equip.equip_pieces))
     info.update(dict(own_script=mm.script.own_script))
+    info.update(dict(directors=mm.director.directors))
 
     # item_info = dict(item=mm.item.items, gitem=mm.grade_item.items,
     #                  citem=mm.coll_item.items, ggitem=mm.guild_gift_item.items,
