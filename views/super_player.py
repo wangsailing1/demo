ea241c0a940c3b0a6ac374b9a9b7d79ee3b35d97
jianmin.liu <jianmin.liu@kaiqigu.cn>
2019-02-28 16:04:09 +0800

    超级大玩家

--- views/super_player.py
+++ views/super_player.py
@@ -6,18 +6,18 @@ from logics.super_player import SuperPlayer
 from lib.utils.debug import print_log
 
 
-def index(env):
+def index(hm):
     """
     首页
 
     """
-    user = env.user
-    super_user = SuperPlayer(user)
+    mm = hm.mm
+    super_user = SuperPlayer(mm)
 
     rc = super_user.is_enter()
     if rc != 0:
         return rc, {}
-    print_log('super_user',user.superplayer.version)
+    print_log('super_user',mm.superplayer.version)
 
     rc, data = super_user.index()
     if rc != 0:
@@ -25,16 +25,16 @@ def index(env):
     return 0, data
 
 
-def buy_goods(env):
+def buy_goods(hm):
     # 购买商品
-    user = env.user
-    super_user = SuperPlayer(user)
+    mm = hm.mm
+    super_user = SuperPlayer(mm)
 
     rc = super_user.is_enter()
     if rc != 0:
         return rc, {}
-    sort_id = int(env.get_argument('sort_id', 0))
-    good_id = int(env.get_argument('good_id', 0))
+    sort_id = hm.get_argument('sort_id', 0, is_int=True)
+    good_id = hm.get_argument('good_id', 0, is_int=True)
     if sort_id <= 0 or sort_id > 3:
         return -1, {}  # 参数错误
 
@@ -42,14 +42,14 @@ def buy_goods(env):
     return rc, data
 
 
-def get_reward(env):
-    user = env.user
-    step_id = int(env.get_argument('step_id', 0))
-    reward_id = int(env.get_argument('reward_id', 0))
+def get_reward(hm):
+    mm = hm.mm
+    step_id = hm.get_argument('step_id', 0, is_int=True)
+    reward_id = hm.get_argument('reward_id', 0, is_int=True)
     if not step_id or not reward_id:
         return -1, {}  # 没有要领取的成就id
 
-    super_user = SuperPlayer(user)
+    super_user = SuperPlayer(mm)
     rc = super_user.is_enter()
     if rc != 0:
         return rc, {}
@@ -58,26 +58,26 @@ def get_reward(env):
     return rc, data
 
 
-def get_bag_code(env):
-    user = env.user
-    super_user = SuperPlayer(user)
+def get_bag_code(hm):
+    mm = hm.mm
+    super_user = SuperPlayer(mm)
     rc = super_user.is_enter()
     if rc != 0:
         return rc, {}
     redbag_info = super_user.get_red_bag()
     return 0, {'redbag_info': redbag_info,
-               'grep_time': user.superplayer.get_time,
-               'can_receive_times': user.superplayer.can_receive_times,
+               'grep_time': mm.superplayer.get_time,
+               'can_receive_times': mm.superplayer.can_receive_times,
             }
 
 
-def grab_bag(env):
-    user = env.user
-    bag_code = env.get_argument('bag_code', '')
+def grab_bag(hm):
+    mm = hm.mm
+    bag_code = hm.get_argument('bag_code', '')
     if not bag_code:
         return -1, {}  # 没有要领取的红包code
 
-    super_user = SuperPlayer(user)
+    super_user = SuperPlayer(mm)
     rc = super_user.is_enter()
     if rc != 0:
         return rc, {}
@@ -85,9 +85,9 @@ def grab_bag(env):
     return rc, data
 
 
-def get_rank_info(env):
-    user = env.user
-    super_user = SuperPlayer(user)
+def get_rank_info(hm):
+    mm = hm.mm
+    super_user = SuperPlayer(mm)
     rc = super_user.is_enter()
     if rc != 0:
         return rc, {}
