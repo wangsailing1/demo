932d5e8d791e59948d498908332e51e1c20eea65
songminger@gmail.com <songminger@gmail.com>
2019-03-08 21:31:34 +0800

    format

--- env_config/song.py
+++ env_config/song.py
@@ -39,6 +39,8 @@ CURRENCY_TYPE = u'RMB'  # 货币类型
 
 RPC_SERVER_ADDR = ('localhost', 8000)
 
+resource = 'http://%s/%s/lr' % (MASTER_HOST, URL_PARTITION)
+payment_callback_url = 'http://%s/%s' % (MASTER_HOST, URL_PARTITION)
 
 DINGTALK_URL = 'https://oapi.dingtalk.com/robot/send?access_token=65b10b52e5038db8362f6d6334df207dccf95410a5e74c99e64ac5dc1152a761'
 
@@ -74,10 +76,6 @@ apps = [
     ('gtt4', 'net1', '127.0.0.1', 6301, 3),
     ('gtt5', 'net1', '127.0.0.1', 6301, 3),
 ]
-
-
-resource = 'http://%s/%s/lr' % (MASTER_HOST, URL_PARTITION)
-payment_callback_url = 'http://%s/%s' % (MASTER_HOST, URL_PARTITION)
 
 PAYMENT_CONFIG = {
     'host': '127.0.0.1',
