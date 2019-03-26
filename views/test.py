8865086b999afbc66c690ea664236cb1a0894fbf
songminger@gmail.com <songminger@gmail.com>
2019-03-26 10:29:41 +0800

    check config mapping

--- views/test.py
+++ views/test.py
@@ -9,7 +9,7 @@ Created on 2017-06-29
 from logics.user import UserLogic
 from lib.core.environ import ModelManager
 from lib.toCpp.interface import lua_battle
-
+from gconfig import game_config
 
 def test_ok(hm):
     return 0, {}
@@ -22,3 +22,10 @@ def test_mail(hm):
 
 def test_return_error(hm):
     return 0, 0
+
+
+def check_config_mapping(hm):
+    """查看配置中间转换mapping状态"""
+    name = hm.get_argument('name', '')
+    return 0, {'name': getattr(game_config, name)}
+
