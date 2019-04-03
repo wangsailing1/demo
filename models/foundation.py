e2d2b721f698342e07dfb53dcfc2f174965005db
jianmin.liu <jianmin.liu@kaiqigu.cn>
2019-04-04 05:01:17 +0800

    修改基金

--- models/foundation.py
+++ models/foundation.py
@@ -7,7 +7,6 @@ from lib.db import ModelBase
 from gconfig import game_config
 from lib.core.environ import ModelManager
 from lib.utils.active_inreview_tools import get_version_by_active_id
-import datetime
 
 
 # 钻石福利基金
@@ -88,20 +87,12 @@ class Foundation(ModelBase):
             if foundation_info['version'] != self.version:
                 continue
             id = foundation_info['id']
-            f_active_date = self.activate_mark[id]
-            f_active_date = datetime.datetime.strptime(f_active_date, '%Y-%m-%d').date()
-            days = (datetime.date.today() - f_active_date).days + 1
-            for d in range(1, days):
-                if d in self.reward_dict.get(id, []):
-                    self.reward_dict[id].remove(d)
             if self.score >= foundation_info['need_coin'] and id not in self.reward_dict:
                 self.activate_mark[id] = time.strftime('%F')
                 reward_dict = []
                 for key, value in foundation_info.iteritems():
                     if key.startswith('day'):
                         day = int(key.split('day')[1])
-                        # if day < days:
-                        #     continue
                         reward_dict.append(day)
                 self.reward_dict[id] = sorted(reward_dict)
 
