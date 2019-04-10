a91834c3e906511dd5dcbbab3d8cf317dd831b0a
songminger@gmail.com <songminger@gmail.com>
2019-04-10 20:18:20 +0800

    本地屏蔽ali bdc

--- views/test.py
+++ views/test.py
@@ -10,6 +10,7 @@ from logics.user import UserLogic
 from lib.core.environ import ModelManager
 from lib.toCpp.interface import lua_battle
 from gconfig import game_config
+from lib.utils.time_tools import timestamp_cache
 
 def test_ok(hm):
     return 0, {}
@@ -28,4 +29,9 @@ def check_config_mapping(hm):
     """查看配置中间转换mapping状态"""
     name = hm.get_argument('name', '')
     return 0, {'name': getattr(game_config, name)}
+
+
+def check_time_lru(hm):
+    return 0, {'timestamp_cache': timestamp_cache.cache}
+
 
