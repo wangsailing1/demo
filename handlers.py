b1c33b632ded4c75ddf0f55dea3e7a9422e780ae
jianmin.liu <jianmin.liu@kaiqigu.cn>
2019-04-12 10:58:43 +0800

    新手引导model从user分离

--- handlers.py
+++ handlers.py
@@ -422,7 +422,7 @@ class APIRequestHandler(BaseRequestHandler):
             old_data = {}
 
             if self.hm.mm:
-                if method_param not in ['user.get_red_dot', 'user.game_info']:
+                if method_param not in ['user.get_red_dot', 'user.game_info', 'user.guide']:
                     cur_lan_sort = self.get_argument('lan', '1')
                     if cur_lan_sort != self.hm.mm.user.language_sort:
                         self.hm.mm.user.language_sort = cur_lan_sort
@@ -480,8 +480,6 @@ class APIRequestHandler(BaseRequestHandler):
                     import traceback
                     print_log(traceback.print_exc())
 
-                if method_param == 'user.guide':
-                    self.hm.mm._model.pop('%s_user' % self.hm.mm.uid)
                 # 执行成功保存数据
                 self.hm.mm.do_save()
 
