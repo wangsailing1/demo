57f02c4b38ef43e45a2118ae5032dcdcab0d10b2
jianmin.liu <jianmin.liu@kaiqigu.cn>
2019-04-23 18:43:36 +0800

    修改推图扫荡

--- views/chapter_stage.py
+++ views/chapter_stage.py
@@ -49,11 +49,11 @@ def open_actor_chat(hm):
 # 扫荡
 def auto_sweep(hm):
     mm = hm.mm
-    if not mm.assistant.assistant:
-        return 'error_assistant', {}  # 请先聘请终身助理
     stage = hm.get_argument('stage', '')
     times = hm.get_argument('times', 1, is_int=True)
     type_hard = hm.get_argument('type_hard', 0, is_int=True)
+    if not mm.assistant.assistant and times == 1:
+        return 'error_assistant', {}  # 请先聘请终身助理
     align = hm.get_argument('align', '')
     if times == 10 and not chapterstage_fastten(mm.user):
         lv = unlock_func_lv('chapterstage_fastten')
@@ -61,3 +61,4 @@ def auto_sweep(hm):
     chapter_stage = Chapter_stage(mm)
     rc, data = chapter_stage.chapter_stage_fight_new(stage, type_hard, auto=True, times=times, align=align)
     return rc, data
+
