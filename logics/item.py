4a9300f13b35916c72cf2e46bc32fc2346310b54
jianmin.liu <jianmin.liu@kaiqigu.cn>
2019-05-05 15:39:48 +0800

    修改bug

--- logics/item.py
+++ logics/item.py
@@ -68,7 +68,7 @@ class ItemLogic(object):
                 return rc, {}
             reward = add_gift(self.mm, 18, [[use_effect, item_num]], cur_data=reward)
         elif is_use == 21: # 随机奖励道具箱
-            for _ in item_num:
+            for _ in range(item_num):
                 gift = self.item.get_random_reward(use_effect[0])
                 reward = add_mult_gift(self.mm, gift , cur_data=reward)
         else:
